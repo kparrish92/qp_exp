e3d4b5c923f645ddf18e1a0116d8486465a74fbe
jvcasillas <joseph.casillas@rutgers.edu>
2020-10-09 17:14:16 -0400

    fix vowels and stops

--- test_script/check_pilot_data.R
+++ test_script/check_pilot_data.R
@@ -2,19 +2,33 @@ library("tidyverse")
 library("here")
 
 
-temp_df <- read_csv(here("data", "em01p_2020_Oct_09_1451.csv")) %>% 
+temp_vowel_df <- read_csv(here("data", "em01p_2020_Oct_09_1451.csv")) %>% 
   filter(!is.na(resp_2.keys) | !is.na(resp_3.keys)) %>% 
-  mutate(exp = if_else(is.na(resp_2.keys), "vowels", "stops")) %>% 
+  mutate(exp = if_else(is.na(resp_2.keys), "stops", "vowels")) %>% 
   separate(stimuli, into = c("trash1", "step"), sep = "_") %>% 
   separate(step, into = c("continuum", "trash2"), sep = 2) %>% 
   mutate(step_cont = as.numeric(continuum), 
-         resp = if_else(resp_2.keys == "left", 0, 1))
+         resp_vowel = if_else(resp_2.keys == "left", 0, 1))
 
-glimpse(temp_df)
+temp_stops_df <- read_csv(here("data", "em01p_2020_Oct_09_1451.csv")) %>% 
+  filter(!is.na(resp_3.keys)) %>% 
+  mutate(exp = "stops") %>% 
+  separate(stimuli, into = c("trash1", "step"), sep = "/") %>% 
+  separate(step, into = c("trash", "step2"), sep = 2) %>% 
+  separate(step2, into = c("step3", "trash"), sep = 3) %>% 
+  mutate(step_cont = as.numeric(step3), 
+         resp_stop = if_else(resp_3.keys == "left", 1, 0))
 
-temp_df %>% 
-  filter(exp == "stops") %>% 
-  ggplot(., aes(x = step_cont, y = resp)) + 
+temp_vowel_df %>% 
+  filter(exp == "vowels") %>% 
+  ggplot(., aes(x = step_cont, y = resp_vowel)) + 
+    geom_hline(yintercept = 0.5, size = 3.2, color = "white") + 
+    geom_jitter(height = 0.02, alpha = 0.3) + 
+    geom_smooth(method = "glm", method.args = list(family = "binomial"), 
+      formula = 'y ~ x')
+
+temp_stops_df %>% 
+  ggplot(., aes(x = step_cont, y = resp_stop)) + 
     geom_hline(yintercept = 0.5, size = 3.2, color = "white") + 
     geom_jitter(height = 0.02, alpha = 0.3) + 
     geom_smooth(method = "glm", method.args = list(family = "binomial"), 
