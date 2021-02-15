e8bd6c661b972ad741b3b09dc5a6276a5b306cb2
jvcasillas <joseph.casillas@rutgers.edu>
2021-02-15 14:30:54 -0500

    add libs, cleanup tidy2afc, add tidy data directory and save initial csv's there.

--- 01_tidy2afc.R
+++ 01_tidy2afc.R
@@ -1,159 +1,142 @@
-
 # Tidy 2afc task --------------------------------------------------------------
+#
+# Last update: 2021-02-15
+# Describe what this script does here.
+#
+# -----------------------------------------------------------------------------
 
 
-# English data 
-english = dir_ls(here("raw_data", "english_2afc"), regexp = "\\.csv$") %>%
-  map_dfr(read_csv, .id = "source")
+# Source libs -----------------------------------------------------------------
+
+source(here::here("00_libs.R"))
+
+# -----------------------------------------------------------------------------
 
 
 
-english = english %>% 
-  select(stimuli, participant, date, expName, resp_2.keys, resp_3.keys) %>% 
-  mutate(exp = if_else(is.na(resp_2.keys), "stops", "vowels")) %>% 
+
+# Helper functions ------------------------------------------------------------
+
+# This is the tidying pipeline that you essentially repeat for every 
+# language dataset. 
+# Since you are going to be repeating it over and over again, it makes 
+# sense to turn it into a function. 
+
+tidy_2afc <- . %>% 
+  map_dfr(read_csv, .id = "source", col_types = cols(.default = "c")) %>% 
+  select(step_cont = stimuli, participant, date, expName, resp_2.keys, resp_3.keys) %>% 
+  mutate(exp = if_else(is.na(resp_2.keys), "stops", "vowels"), 
+         step_cont = str_remove(step_cont, "stim/VC_"), 
+         step_cont = str_remove(step_cont, "stim2/pa"), 
+         step_cont = str_remove(step_cont, "\\+"), 
+         step_cont = str_remove(step_cont, ".wav"), 
+         step_cont = as.numeric(step_cont), 
+         response = case_when(
+           resp_2.keys == "left"  ~ 0, 
+           resp_2.keys == "right" ~ 1, 
+           resp_3.keys == "left"  ~ 1, 
+           resp_3.keys == "right" ~ 0)) %>% 
+  filter(nchar(as.character(participant)) == 24)
+
+# -----------------------------------------------------------------------------
+
+
+
+
+# Tidy English data -----------------------------------------------------------
+
+english <- dir_ls(here("raw_data", "english_2afc"), regexp = "\\.csv$") %>%
+  tidy_2afc() %>% 
   mutate(language = "english")
 
-english_vowels = english %>% 
+english_vowels <- english %>% 
   filter(!is.na(resp_2.keys)) %>% 
-  filter(nchar(as.character(participant))==24) %>% 
-  separate(stimuli, into = c("trash1", "step"), sep = "_") %>% 
-  separate(step, into = c("continuum", "trash2"), sep = 2) %>% 
-  mutate(step_cont = as.numeric(continuum),
-         resp_vowel = if_else(resp_2.keys == "left", 0, 1)) %>% 
-  select(participant, date, expName, resp_2.keys, exp, language, step_cont, resp_vowel)
+  select(participant, date, expName, resp_2.keys, exp, language, 
+         step_cont, response)
 
-
-english_stops = english %>% 
+english_stops <- english %>% 
   filter(!is.na(resp_3.keys)) %>% 
-  filter(nchar(as.character(participant))==24) %>% 
-  separate(stimuli, into = c("trash1", "step"), sep = "/") %>% 
-  separate(step, into = c("trash", "step2"), sep = 2) %>% 
-  separate(step2, into = c("step3", "trash"), sep = 3) %>% 
-  mutate(step_cont = as.numeric(step3), 
-         resp_stop = if_else(resp_3.keys == "left", 1, 0)) %>% 
-  select(participant, date, expName, resp_2.keys, exp, language, step_cont, resp_stop)
+  select(participant, date, expName, resp_2.keys, exp, language, 
+         step_cont, response)
+
+# -----------------------------------------------------------------------------
 
 
-### Spanish Data
-  
-spanish <-
-  list.files(path = "./raw_data/spanish_2afc",
-             pattern = "*.csv", 
-             full.names = T) %>% 
-  map_df(~read_csv(., col_types = cols(.default = "c"))) 
-  
-warnings()
-  
-spanish = spanish %>% 
-  select(stimuli, participant, date, expName, resp_2.keys, resp_3.keys) %>% 
-  mutate(exp = if_else(is.na(resp_2.keys), "stops", "vowels")) %>% 
+
+# Tidy Spanish Data -----------------------------------------------------------
+
+spanish <- dir_ls(here("raw_data", "spanish_2afc"), regexp = "\\.csv$") %>% 
+  tidy_2afc() %>% 
   mutate(language = "spanish")
 
-spanish_vowels = spanish %>% 
+spanish_vowels <- spanish %>% 
   filter(!is.na(resp_2.keys)) %>% 
-  filter(nchar(as.character(participant))==24) %>% 
-  separate(stimuli, into = c("trash1", "step"), sep = "_") %>% 
-  separate(step, into = c("continuum", "trash2"), sep = 2) %>% 
-  mutate(step_cont = as.numeric(continuum),
-         resp_vowel = if_else(resp_2.keys == "left", 0, 1)) %>% 
-  select(participant, date, expName, resp_2.keys, exp, language, step_cont, resp_vowel)
+  select(participant, date, expName, resp_2.keys, exp, language, 
+         step_cont, response)
 
-
-spanish_stops = spanish %>% 
+spanish_stops <- spanish %>% 
   filter(!is.na(resp_3.keys)) %>% 
-  filter(nchar(as.character(participant))==24) %>% 
-  separate(stimuli, into = c("trash1", "step"), sep = "/") %>% 
-  separate(step, into = c("trash", "step2"), sep = 2) %>% 
-  separate(step2, into = c("step3", "trash"), sep = 3) %>% 
-  mutate(step_cont = as.numeric(step3), 
-         resp_stop = if_else(resp_3.keys == "left", 1, 0)) %>% 
-  select(participant, date, expName, resp_2.keys, exp, language, step_cont, resp_stop)
+  select(participant, date, expName, resp_3.keys, exp, language, 
+         step_cont, response)
+
+# -----------------------------------------------------------------------------
 
 
 
 
-## French data 
+## Tidy French data -----------------------------------------------------------
 
-
-
-french = dir_ls(here("raw_data", "french_2afc"), regexp = "\\.csv$") %>%
-  map_dfr(read_csv, .id = "source")
-
-
-french = french %>% 
-  select(stimuli, participant, date, expName, resp_2.keys, resp_3.keys) %>% 
-  mutate(exp = if_else(is.na(resp_2.keys), "stops", "vowels")) %>% 
+french <- dir_ls(here("raw_data", "french_2afc"), regexp = "\\.csv$") %>%
+  tidy_2afc() %>% 
   mutate(language = "french")
 
-french_vowels = french %>% 
+french_vowels <- french %>% 
   filter(!is.na(resp_2.keys)) %>% 
-  filter(nchar(as.character(participant))==24) %>% 
-  separate(stimuli, into = c("trash1", "step"), sep = "_") %>% 
-  separate(step, into = c("continuum", "trash2"), sep = 2) %>% 
-  mutate(step_cont = as.numeric(continuum),
-         resp_vowel = if_else(resp_2.keys == "left", 0, 1)) %>% 
-  select(participant, date, expName, resp_2.keys, exp, language, step_cont, resp_vowel)
+  select(participant, date, expName, resp_2.keys, exp, language, 
+         step_cont, response)
 
-
-french_stops = french %>% 
+french_stops <- french %>% 
   filter(!is.na(resp_3.keys)) %>% 
-  filter(nchar(as.character(participant))==24) %>% 
-  separate(stimuli, into = c("trash1", "step"), sep = "/") %>% 
-  separate(step, into = c("trash", "step2"), sep = 2) %>% 
-  separate(step2, into = c("step3", "trash"), sep = 3) %>% 
-  mutate(step_cont = as.numeric(step3), 
-         resp_stop = if_else(resp_3.keys == "left", 1, 0)) %>% 
-  select(participant, date, expName, resp_2.keys, exp, language, step_cont, resp_stop)
+  select(participant, date, expName, resp_3.keys, exp, language, 
+         step_cont, response)
+
+# -----------------------------------------------------------------------------
 
 
-## Hungarian data 
-
-hungarian <-
-  list.files(path = "./raw_data/hungarian_2afc",
-             pattern = "*.csv", 
-             full.names = T) %>% 
-  map_df(~read_csv(., col_types = cols(.default = "c"))) 
 
 
-hungarian = hungarian %>% 
-  select(stimuli, participant, date, expName, resp_2.keys, resp_3.keys) %>% 
-  mutate(exp = if_else(is.na(resp_2.keys), "stops", "vowels")) %>% 
+# Tidy Hungarian data ---------------------------------------------------------
+
+hungarian <- dir_ls(here("raw_data", "hungarian_2afc"), regexp = "\\.csv$") %>% 
+  tidy_2afc() %>% 
   mutate(language = "hungarian")
 
-hungarian_vowels = hungarian %>% 
+hungarian_vowels <- hungarian %>% 
   filter(!is.na(resp_2.keys)) %>% 
-  filter(nchar(as.character(participant))==24) %>% 
-  separate(stimuli, into = c("trash1", "step"), sep = "_") %>% 
-  separate(step, into = c("continuum", "trash2"), sep = 2) %>% 
-  mutate(step_cont = as.numeric(continuum),
-         resp_vowel = if_else(resp_2.keys == "left", 0, 1)) %>% 
-  select(participant, date, expName, resp_2.keys, exp, language, step_cont, resp_vowel)
+  select(participant, date, expName, resp_2.keys, exp, language, 
+         step_cont, response)
 
-
-hungarian_stops = hungarian %>% 
+hungarian_stops <- hungarian %>% 
   filter(!is.na(resp_3.keys)) %>% 
-  filter(nchar(as.character(participant))==24) %>% 
-  separate(stimuli, into = c("trash1", "step"), sep = "/") %>% 
-  separate(step, into = c("trash", "step2"), sep = 2) %>% 
-  separate(step2, into = c("step3", "trash"), sep = 3) %>% 
-  mutate(step_cont = as.numeric(step3), 
-         resp_stop = if_else(resp_3.keys == "left", 1, 0)) %>% 
-  select(participant, date, expName, resp_2.keys, exp, language, step_cont, resp_stop)
+  select(participant, date, expName, resp_3.keys, exp, language, 
+         step_cont, response)
 
-
-
-## Combine them all 
-all_stops = rbind(english_stops, hungarian_stops, spanish_stops, french_stops)
-all_vowels = rbind(english_vowels, spanish_vowels, french_vowels, hungarian_vowels)
+# -----------------------------------------------------------------------------
 
 
 
 
+# Combine data ----------------------------------------------------------------
 
+all_stops  <- bind_rows(
+  english_stops, hungarian_stops, spanish_stops, french_stops) %>% 
+  write_csv(here("data", "tidy", "2afc_stops_tidy.csv"))
 
+all_vowels <- bind_rows(
+  english_vowels, spanish_vowels, french_vowels, hungarian_vowels) %>% 
+  write_csv(here("data", "tidy", "2afc_vowels_tidy.csv"))
 
+complete_2afc <- bind_rows(all_stops, all_vowels) %>% 
+  write_csv(here("data", "tidy", "2afc_complete_tidy.csv"))
 
-
-  
-  
-
+# -----------------------------------------------------------------------------
