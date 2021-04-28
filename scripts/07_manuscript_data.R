2e81f43b660e6790446d86f1fe70cab214ba11d1
kparrish92 <60477545+kparrish92@users.noreply.github.com>
2021-04-28 14:14:18 -0400

    updating results

--- scripts/07_manuscript_data.R
+++ scripts/07_manuscript_data.R
@@ -238,13 +238,239 @@ eng_lextale = all_lextale_tidy %>%
 span_lextale = all_lextale_tidy %>% 
   filter(!is.na(Spanish))
 
+# objects for proficiency tost 
 
 englexmean = mean(eng_lextale$English)
 englexsd = sd(eng_lextale$English)
 spanlexmean = mean(span_lextale$Spanish)
 spanlexsd = sd(span_lextale$Spanish)
 
-
+## Proficiency tost 
 tost_prof = TOSTtwo(m1 = spanlexmean, m2 = englexmean, sd1 = spanlexsd, sd2 = englexsd, n1 = 25, n2 = 47, 
-        low_eqbound_d = -0.4929019, high_eqbound_d = 0.4929019, alpha = 0.05, 
+        low_eqbound_d = -.1, high_eqbound_d = .1, alpha = 0.05, 
         var.equal = FALSE)
+
+# ----------------------------------------------------------------------------------------
+# SE stops 
+
+boundary_diff_sl1s = mean(q0_stops_s$english) - mean(q0_stops_s$spanish)
+
+tost_l3_spanl1_stops_se = TOSTpaired(n = 46, m1 = mean(spanish_pooled_stops$english), 
+                                  m2 = mean(spanish_pooled_stops$spanish),
+                                  sd1 = sd(spanish_pooled_stops$english),
+                                  sd2 = sd(spanish_pooled_stops$spanish),
+                                  r12 = cor(spanish_pooled_stops$english, spanish_pooled_stops$spanish), 
+                                  low_eqbound_dz = -.5,
+                                  high_eqbound_dz = .5,
+                                  alpha = .05, plot = TRUE, verbose = TRUE)
+
+tost_l3_spanl1_stops_el3 = TOSTpaired(n = 46, m1 = mean(spanish_pooled_stops$english), 
+                                     m2 = mean(spanish_pooled_stops$l3),
+                                     sd1 = sd(spanish_pooled_stops$english),
+                                     sd2 = sd(spanish_pooled_stops$l3),
+                                     r12 = cor(spanish_pooled_stops$english, spanish_pooled_stops$l3), 
+                                     low_eqbound_dz = -.5,
+                                     high_eqbound_dz = .5,
+                                     alpha = .05, plot = TRUE, verbose = TRUE)
+
+
+tost_l3_spanl1_stops_sl3 = TOSTpaired(n = 46, m1 = mean(spanish_pooled_stops$l3), 
+                                     m2 = mean(spanish_pooled_stops$spanish),
+                                     sd1 = sd(spanish_pooled_stops$l3),
+                                     sd2 = sd(spanish_pooled_stops$spanish),
+                                     r12 = cor(spanish_pooled_stops$spanish, spanish_pooled_stops$l3), 
+                                     low_eqbound_dz = -.5,
+                                     high_eqbound_dz = .5,
+                                     alpha = .05, plot = TRUE, verbose = TRUE)
+
+tost_l3_spanl1_stops_se = (as.data.frame(tost_l3_spanl1_stops_se)) %>% 
+  mutate(combination = "Spanish English")
+
+tost_l3_spanl1_stops_el3 = (as.data.frame(tost_l3_spanl1_stops_el3)) %>% 
+  mutate(combination = "English L3")
+
+tost_l3_spanl1_stops_sl3 = (as.data.frame(tost_l3_spanl1_stops_sl3)) %>% 
+  mutate(combination = "Spanish L3")
+
+all_tost_spanl1_stops = rbind(tost_l3_spanl1_stops_sl3,
+                               tost_l3_spanl1_stops_el3,
+                               tost_l3_spanl1_stops_se)                                   
+
+all_tost_spanl1_stops %>% 
+  ggplot(aes(x = diff, y = combination)) + geom_point() + 
+  geom_linerange(aes(xmin = LL_CI_TOST, xmax = UL_CI_TOST)) +
+  geom_vline(xintercept = all_tost_spanl1_stops$low_eqbound, linetype = "dashed") +
+  geom_vline(xintercept = all_tost_spanl1_stops$high_eqbound, linetype = "dashed") +
+  ggtitle("Spanish L1 stops")
+
+
+
+# ----------------------------------------------------------------------------------------
+
+# determine upper and lower bounds based on SE boundary of this group 
+
+
+boundary_diff_sl1v = mean(q0_vowels_s$english) - mean(q0_vowels_s$spanish)
+
+
+tost_l3_spanl1_vowels_se = TOSTpaired(n = 46, m1 = mean(spanish_pooled_vowels$english), 
+                                     m2 = mean(spanish_pooled_vowels$spanish),
+                                     sd1 = sd(spanish_pooled_vowels$english),
+                                     sd2 = sd(spanish_pooled_vowels$spanish),
+                                     r12 = cor(spanish_pooled_vowels$english, spanish_pooled_vowels$spanish), 
+                                     low_eqbound_dz = -.5,
+                                     high_eqbound_dz = .5,
+                                     alpha = .05, plot = TRUE, verbose = TRUE)
+
+tost_l3_spanl1_vowels_el3 = TOSTpaired(n = 46, m1 = mean(spanish_pooled_vowels$english), 
+                                      m2 = mean(spanish_pooled_vowels$l3),
+                                      sd1 = sd(spanish_pooled_vowels$english),
+                                      sd2 = sd(spanish_pooled_vowels$l3),
+                                      r12 = cor(spanish_pooled_vowels$english, spanish_pooled_vowels$l3), 
+                                      low_eqbound_dz = -.5,
+                                      high_eqbound_dz = .5,
+                                      alpha = .05, plot = TRUE, verbose = TRUE)
+
+
+tost_l3_spanl1_vowels_sl3 = TOSTpaired(n = 46, m1 = mean(spanish_pooled_vowels$l3), 
+                                      m2 = mean(spanish_pooled_vowels$spanish),
+                                      sd1 = sd(spanish_pooled_vowels$l3),
+                                      sd2 = sd(spanish_pooled_vowels$spanish),
+                                      r12 = cor(spanish_pooled_vowels$spanish, spanish_pooled_vowels$l3), 
+                                      low_eqbound_dz = -.5,
+                                      high_eqbound_dz = .5,
+                                      alpha = .05, plot = TRUE, verbose = TRUE)
+
+
+tost_l3_spanl1_vowels_sl3_df = (as.data.frame(tost_l3_spanl1_vowels_sl3)) %>% 
+  mutate(combination = "Spanish L3")
+                                   
+tost_l3_spanl1_vowels_el3 = (as.data.frame(tost_l3_spanl1_vowels_el3)) %>% 
+  mutate(combination = "English L3")
+
+tost_l3_spanl1_vowels_se = (as.data.frame(tost_l3_spanl1_vowels_se)) %>% 
+  mutate(combination = "Spanish English")
+
+all_tost_spanl1_vowels = rbind(tost_l3_spanl1_vowels_sl3_df,
+                               tost_l3_spanl1_vowels_el3,
+                               tost_l3_spanl1_vowels_se)                                   
+
+all_tost_spanl1_vowels %>% 
+  ggplot(aes(x = diff, y = combination)) + geom_point() + 
+  geom_linerange(aes(xmin = LL_CI_TOST, xmax = UL_CI_TOST)) +
+  geom_vline(xintercept = all_tost_spanl1_vowels$low_eqbound, linetype = "dashed") +
+  geom_vline(xintercept = all_tost_spanl1_vowels$high_eqbound, linetype = "dashed") + 
+  ggtitle("Spanish L1 Vowels")
+  
+
+
+
+# ---------------------------------------------------------------------------------------
+# ES stops 
+
+
+boundary_diff_el1s = mean(q0_stops_e$english) - mean(q0_stops_e$spanish)
+
+
+tost_l3_engl1_stops_se = TOSTpaired(n = 23, m1 = mean(english_pooled_stops$english), 
+                                     m2 = mean(english_pooled_stops$spanish),
+                                     sd1 = sd(english_pooled_stops$english),
+                                     sd2 = sd(english_pooled_stops$spanish),
+                                     r12 = cor(english_pooled_stops$english, english_pooled_stops$spanish), 
+                                     low_eqbound_dz = -.5,
+                                     high_eqbound_dz = .5,
+                                     alpha = .05, plot = TRUE, verbose = TRUE)
+
+tost_l3_engl1_stops_el3 = TOSTpaired(n = 23, m1 = mean(english_pooled_stops$english), 
+                                      m2 = mean(english_pooled_stops$l3),
+                                      sd1 = sd(english_pooled_stops$english),
+                                      sd2 = sd(english_pooled_stops$l3),
+                                      r12 = cor(english_pooled_stops$english, english_pooled_stops$l3), 
+                                      low_eqbound_dz = -.5,
+                                      high_eqbound_dz = .5,
+                                      alpha = .05, plot = TRUE, verbose = TRUE)
+
+
+tost_l3_engl1_stops_sl3 = TOSTpaired(n = 23, m1 = mean(english_pooled_stops$l3), 
+                                      m2 = mean(english_pooled_stops$spanish),
+                                      sd1 = sd(english_pooled_stops$l3),
+                                      sd2 = sd(english_pooled_stops$spanish),
+                                      r12 = cor(english_pooled_stops$spanish, english_pooled_stops$l3), 
+                                      low_eqbound_dz = -.5,
+                                      high_eqbound_dz = .5,
+                                      alpha = .05, plot = TRUE, verbose = TRUE)
+# compiling tosts to dfs and binding for plot 
+tost_l3_engl1_stops_se = (as.data.frame(tost_l3_engl1_stops_se)) %>% 
+  mutate(combination = "Spanish English")
+
+tost_l3_engl1_stops_el3 = (as.data.frame(tost_l3_engl1_stops_el3)) %>% 
+  mutate(combination = "English L3")
+
+tost_l3_engl1_stops_sl3 = (as.data.frame(tost_l3_engl1_stops_sl3)) %>% 
+  mutate(combination = "Spanish L3")
+
+all_tost_engl1_stops = rbind(tost_l3_engl1_stops_se,
+                              tost_l3_engl1_stops_el3,
+                              tost_l3_engl1_stops_sl3)                                   
+
+all_tost_engl1_stops %>% 
+  ggplot(aes(x = diff, y = combination)) + geom_point() + 
+  geom_linerange(aes(xmin = LL_CI_TOST, xmax = UL_CI_TOST)) +
+  geom_vline(xintercept = all_tost_engl1_stops$low_eqbound, linetype = "dashed") +
+  geom_vline(xintercept = all_tost_engl1_stops$high_eqbound, linetype = "dashed") +
+  ggtitle("English L1 stops")
+
+sl1vowelscomb = rbind(q0_vowels_sh, q0_vowels_sf)
+
+
+
+# non-paired TOST between L3 groups 
+
+# Spanish l1 vowels
+tost_L3_spanv = TOSTtwo(m1 = mean(q0_vowels_sh$l3), m2 = mean(q0_vowels_sf$l3), 
+                    sd1 = sd(q0_vowels_sh$l3), sd2 = sd(q0_vowels_sf$l3), 
+                    n1 = 28, n2 = 17, 
+                    low_eqbound_d = -.5, 
+                    high_eqbound_d = .5, 
+                    alpha = 0.05,
+                    var.equal = FALSE)
+# Spanish l1 stops
+tost_L3_spans = TOSTtwo(m1 = mean(q0_stops_sh$l3), m2 = mean(q0_stops_sf$l3), 
+                        sd1 = sd(q0_stops_sh$l3), sd2 = sd(q0_stops_sf$l3), 
+                        n1 = 29, n2 = 17, 
+                        low_eqbound_d = -.5, 
+                        high_eqbound_d = .5, 
+                        alpha = 0.05,
+                        var.equal = FALSE)
+
+# english l1 stops 
+tost_L3_engs = TOSTtwo(m1 = mean(q0_stops_eh$l3), m2 = mean(q0_stops_ef$l3), 
+                        sd1 = sd(q0_stops_eh$l3), sd2 = sd(q0_stops_ef$l3), 
+                        n1 = 14, n2 = 9, 
+                        low_eqbound_d = -.5, 
+                        high_eqbound_d = .5, 
+                        alpha = 0.05,
+                        var.equal = FALSE)
+
+# compiling tosts to dfs and binding for plot # EDIT
+tost_L3_spanv = (as.data.frame(tost_L3_spanv)) %>% 
+  mutate(combination = "Spanish L1 Vowels")
+
+tost_L3_spans = (as.data.frame(tost_L3_spans)) %>% 
+  mutate(combination = "Spanish L1 Stops")
+
+tost_L3_engs = (as.data.frame(tost_L3_engs)) %>% 
+  mutate(combination = "English L1 Stops")
+
+all_tost_l3groups = rbind(tost_L3_spanv,
+                          tost_L3_spans,
+                          tost_L3_engs)                                   
+
+all_tost_l3groups %>% 
+  ggplot(aes(x = diff, y = combination)) + geom_point() + 
+  geom_linerange(aes(xmin = LL_CI_TOST, xmax = UL_CI_TOST)) +
+  geom_vline(xintercept = all_tost_engl1_stops$low_eqbound, linetype = "dashed") +
+  geom_vline(xintercept = all_tost_engl1_stops$high_eqbound, linetype = "dashed") +
+  ggtitle("Figure X: Comparison of categoriztions between L3 groups")
+
+
